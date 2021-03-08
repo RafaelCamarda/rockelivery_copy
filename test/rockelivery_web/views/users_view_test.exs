c2465f7d7e690a15af17be5912425200a69588e4
Rafael Camarda <rf.camarda@gmail.com>
2021-03-08 15:50:16 -0300

    finish tests

--- test/rockelivery_web/views/users_view_test.exs
+++ test/rockelivery_web/views/users_view_test.exs
@@ -29,4 +29,26 @@ defmodule RockeliveryWeb.UsersViewTest do
              }
            } = response
   end
+
+  test "renders user.json" do
+    user = build(:user)
+
+    response = render(UsersView, "user.json", user: user)
+
+    assert %{
+             user: %User{
+               address: "Rua das banananeiras 15",
+               age: 27,
+               cep: "12345678",
+               cpf: "12345678901",
+               email: "rafael@banana.com",
+               id: "e0cb8256-1eb5-4cc5-8549-4a61671b3d18",
+               inserted_at: nil,
+               name: "Rafael Camarda",
+               password: "123456",
+               password_hash: nil,
+               updated_at: nil
+             }
+           } = response
+  end
 end
