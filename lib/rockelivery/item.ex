c332e95f969eaca64a328f4ed05ad3808f506543
Rafael Camarda <rf.camarda@gmail.com>
2021-03-15 11:50:31 -0300

    add item schema

---
+++ lib/rockelivery/item.ex
@@ -0,0 +1,29 @@
+defmodule Rockelivery.Item do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Ecto.Enum
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params [:category, :description, :price, :photo]
+
+  @derive {Jason.Encoder, only: @required_params ++ [:id]}
+
+  schema "items" do
+    field :category, Enum, values: [:food, :drink, :desert]
+    field :description, :string
+    field :price, :decimal
+    field :photo, :string
+
+    timestamps()
+  end
+
+  def changeset(changeset \\ %__MODULE__{}, params) do
+    changeset
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:description, min: 6)
+    |> validate_number(:price, greater_than: 0)
+  end
+end
