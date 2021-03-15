b3ee88f1c7ce115d68e17d27d563ef69d8cfa361
Rafael Camarda <rf.camarda@gmail.com>
2021-03-15 15:13:29 -0300

    finish item creation

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -4,8 +4,12 @@ defmodule Rockelivery do
   alias Rockelivery.Users.Get, as: UserGet
   alias Rockelivery.Users.Update, as: UserUpdate
 
+  alias Rockelivery.Items.Create, as: ItemCreate
+
   defdelegate create_user(params), to: UserCreate, as: :call
   defdelegate delete_user(params), to: UserDelete, as: :call
   defdelegate update_user(params), to: UserUpdate, as: :call
   defdelegate get_user_by_id(id), to: UserGet, as: :by_id
+
+  defdelegate create_item(params), to: ItemCreate, as: :call
 end
