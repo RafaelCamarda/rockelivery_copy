4369cec06a10f10d8346050b5b2817dcdd1e9329
Rafael Camarda <rf.camarda@gmail.com>
2021-03-22 18:06:39 -0300

    finish client

---
+++ lib/rockelivery/via_cep/client.ex
@@ -0,0 +1,32 @@
+defmodule Rockelivery.ViaCep.Client do
+  use Tesla
+
+  alias Rockelivery.Error
+
+  plug Tesla.Middleware.BaseUrl, "https://viacep.com.br/ws"
+  plug Tesla.Middleware.JSON
+
+  def get_cep_info(cep) do
+    "#{cep}/json/"
+    |> get()
+    |> handle_get()
+  end
+
+  defp handle_get({:ok, %Tesla.Env{status: 200, body: %{"erro" => true}}}) do
+    build_error(:not_found, "CEP not found!")
+  end
+
+  defp handle_get({:ok, %Tesla.Env{status: 200, body: body}}) do
+    {:ok, body}
+  end
+
+  defp handle_get({:ok, %Tesla.Env{status: 400, body: _body}}) do
+    build_error(:bad_request, "Invalid CEP")
+  end
+
+  defp handle_get({:error, reason}) do
+    build_error(:bad_request, reason)
+  end
+
+  defp build_error(status, result), do: {:error, Error.build(status, result)}
+end
